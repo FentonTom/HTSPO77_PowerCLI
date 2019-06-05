2c500958e389c98ad927470f570a10df8e207134
FentonTom <tom.fenton@gmail.com>
2019-06-05 11:16:53 -0700

    Update HTSPO77_Lab03.ps1
    Removed wait-task

--- HTSPO77_Lab03.ps1
+++ HTSPO77_Lab03.ps1
@@ -28,17 +28,18 @@ write-host Breaking System  -foreground green
 # write-host  Connecting to vCenter Server $vCenter -foreground green
 Connect-viserver $vCenter -user $vCenterUser -password $vCenterUserPassword -WarningAction 0
 ## Power off VM that will be cloned if it is powered on
-Get-VMGuest -VM $clone  | Where-Object {$_.State -eq "Running"}  | Shutdown-VMGuest -Confirm:$False
+Get-VMGuest -VM $cloneÂ  | Where-Object {$_.State -eq "Running"} Â | Shutdown-VMGuest -Confirm:$False
 # Shutdown-VMGuest -VM $Clone -Confirm:$false > $null
 ## Clone VM 
 $ESXi=Get-Cluster $Cluster | Get-VMHost -state connected | Get-Random
-New-VM -Name $New_VM_Name -VM $clone -VMHost $ESXi -Datastore $ds -RunAsync > $null
+New-VM -Name $New_VM_Name -VM $clone -VMHost $ESXi -Datastore $ds > $null
+write-host Still breaking System  -foreground green
 
 # Remove orginal VM
 #_______________________________________________________
 #_______________________________________________________
 #
-Remove-VM -VM "$clone" -DeletePermanently -RunAsync -Confirm:$false > $null
+Remove-VM -VM "$clone" -DeletePermanently -Confirm:$false > $null
 
 # Inform student that the lab is broken and ask to continue
 Write-Host "System is Broken Press any key to continue ..." -foreground Yellow
